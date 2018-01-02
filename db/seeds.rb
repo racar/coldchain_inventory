09eef73b78b7df371c4b757c85c11fa003dae089
Rafael Carrascal R <rcarrascal@gmail.com>
2018-01-01 23:46:20 -0500

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 
 AdminUser.delete_all
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
-
+=begin
 Municipio.delete_all
 File.open("#{Rails.root}/db/municipios.txt") do |municipios|
   municipios.read.each_line do |municipio|
@@ -69,38 +69,4 @@ File.open("#{Rails.root}/db/modelos.txt") do |modelos|
     Modelo.create!(:nombre => nombre, :category_id => cat, :marca_id => mar)
   end
 end
-
-#Item.delete_all
-#File.open("#{Rails.root}/db/ref_con.txt") do |refcons|
-#  refcons.read.each_line do |refcon|
-#    municipio,entidad,tipo,marca,modelo,serie,mantenimiento,man_year,regulador,monitoreo = refcon.chomp.split("|")
-#
-#    @cat = Category.where(["nombre = ? ", "Refrigerador"]).first
-#    cat = 0
-#    if @cat
-#      cat = @cat.id
-#    end
-#
-#    @mar = Marca.where(["nombre = ? ", marca]).first
-#    mar = 0
-#    if @mar
-#      mar = @mar.id
-#    end
-#
-#    @mar = Modelo.where(["nombre = ? ", modelo]).first
-#    mod = 0
-#    if @mod
-#      mod = @mod.id
-#    end
-#
-#    @ent = Institucion.where(["name = ? ", entidad]).first
-#    ent = 0
-#    if @ent
-#      ent = @ent.id
-#    end
-#
-#
-#    Item.create!(:institucion_id => ent,:category => cat, :marca => mar, :modelo => mod, :serial => serie, :mantenimiento => mantenimiento, :num_manten_year => man_year, :regulador => regulador, :monitoreo => monitoreo)
-#
-#  end
-#end
+=end
