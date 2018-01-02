ba6bb05b19963c8086a642efc98acd9b0cefc7cf
Rafael Carrascal R <rcarrascal@gmail.com>
2018-01-01 23:37:34 -0500

    activeadmin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 
 
 AdminUser.delete_all
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 
 Municipio.delete_all
 File.open("#{Rails.root}/db/municipios.txt") do |municipios|
